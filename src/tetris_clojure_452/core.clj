dd94318f44b9ddf3acb1c43dedb2181e26e69207
cbw975 <cbw975@users.noreply.github.com>
2020-04-07 12:23:47 -0400

    Tetris Game - board representation helper functs

--- src/tetris_clojure_452/core.clj
+++ src/tetris_clojure_452/core.clj
@@ -7,34 +7,42 @@
 
 ;; For storing game state + string based display to repl
 (def empty-block 0)
-(def active-block 1)
-(def placed-blocks 2)
+(def active-block 1) ; active cells recognized as 1s
+(def placed-block 2) ; filled cells recognized as 2s
 (def world-height 20) ;;world refers to the area tetris blocks can be in
 (def world-width 10)
 
-(def i-block [[0 1 0 0]
-              [0 1 0 0]
-              [0 1 0 0]
-              [0 1 0 0]])
-(def o-block [[1 1]
-              [1 1]])
-(def l-block [[0 1 0]
-              [0 1 0]
-              [0 1 1]])
-(def r-block [[0 1 1]
-              [0 1 0]
-              [0 1 0]])
-(def t-block [[0 0 0]
-              [1 1 1]
-              [0 1 0]])
-(def z-block [[0 0 0]
-              [1 1 0]
-              [0 1 1]])
-(def s-block [[0 0 0]
-              [0 1 1]
-              [1 1 0]])
+(def I [[0 1 0 0]
+        [0 1 0 0]
+        [0 1 0 0]
+        [0 1 0 0]])
+(def O [[1 1]
+        [1 1]])
+(def L [[0 1 0]
+        [0 1 0]
+        [0 1 1]])
+(def R [[0 1 1]
+        [0 1 0]
+        [0 1 0]])
+(def T [[0 0 0]
+        [1 1 1]
+        [0 1 0]])
+(def Z [[0 0 0]
+        [1 1 0]
+        [0 1 1]])
+(def S [[0 0 0]
+        [0 1 1]
+        [1 1 0]])
 
-(def blocks [i-block o-block l-block r-block t-block z-block s-block])
+(def shapes [I O L R T Z S])
+
+(defn start-state []
+  {:score 0 ; score of game. starts at 0
+   ;:speed 100 ; implement later
+   :high-score 0
+   :filled #{} ; set of coordinates of filled cells in world
+   :active-pos [(rand-int world-width) 0] ; top-left coord of active tetris block. randomly somewhere along the top row of world
+   :active-shape ((rand-nth (keys shapes)) shapes)}) ; shape is the current falling/active block
 
 ;;make a row (vector) of specified blocks
 (defn make-row
@@ -45,7 +53,7 @@
   (make-row world-height (make-row))) ;empty rows up to world's height
 
 (defn transpose [matrix]
-  (into [] ;make transposed block structure a vector of rows
+  (into [] ;make transposed shape structure a vector of rows
         (for [row (apply map list matrix)] ;transposes matrix, but structure ends up as list of lists.
           (into [] row)))) ;make each row a vector
 
@@ -54,10 +62,30 @@
   (assoc items i1 (items i2) i2 (items i1))) ; (assoc seq i1 item1 i2 item2) replaces item1 with item2 at index i2 and vice versa
 
 ;; rotates a tetris block (clockwise)
-(defn rotate     ;; TODO: Rotate only if doesn't go out of bounds or collide with other blocks
-  (swap (transpose block) 0 (dec (count block))))
-;; Thought process Note: when a block rotates, each cell "moves" around the center-point (area-length - 1) over. (for i-block, think of middle 1's as own mini-block)
+(defn rotate [shape] ;; TODO: Rotate only if doesn't go out of bounds or collide with other blocks
+  (swap (transpose shape) 0 (dec (count shape))))
+;; Thought process Note: when a shape rotates, each cell "moves" around the center-point (area-length - 1) over. (for i-block, think of middle 1's as own mini-block)
 ;; => Basically if block matrix is a square, can take the transpose of matrix and swap first and last rows
+
+(defn cell-in [shape x y]
+  (get-in shape [x y]))
+
+; deconstruct active shape and return coordinates of its 4 filled cells (1's)
+(defn shape-cells [{:keys [active-pos active-shape]}]
+  (let [d (count active-shape)]
+    (for [x (range d) y (range d)
+          :when (= 1 (cell-in active-shape x y))]
+      (mapv + [i j] active-pos))))
+
+; check if active-block intersects with filled/out-of-bounds blocks
+(defn valid? [{:keys [filled] :as state}]
+  ; for every coordinate in the current active shape (shape-cells), check if within world boundaries and not in the filled cells
+  (every? (fn [[x y :as coord]]
+               (and ((complement filled) coord)
+                    (<= 0 x (- world-width 1)) (< y (- world-height 1))))
+          (shape-cells state)))
+
+(defn place-shape [state])
 
 ; TODO: keyboard events (arrow keys + shift to rotate)
 ; TODO: Check for game over
@@ -69,4 +97,3 @@
 
 (defn gameover? [world]
   )
-
